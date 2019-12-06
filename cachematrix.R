462ccf25e8bd6fc79de054e4814387887b13c696
samuel fabo <sfabo@profinit.eu>
2019-12-06 09:36:23 +0100

    Assignment done.
    Caching works

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Creating cahing mechanism for computing inverse - saving complexity of the program.
 
-## Write a short comment describing this function
+# makeCacheMatrix will save the cache inverse in a matrix format
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+# Once the inverse is computed, it is stored in a memory. 
+# When needed, the cached matrix is returned.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(X, ...)
+    x$setInverse(inv)
+    inv
 }
